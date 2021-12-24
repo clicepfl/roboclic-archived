446bfff831de4ab9676eb8c297f403656c798b09
s314cy <hugo.elguedj@epfl.ch>
2021-12-24 17:51:56 +0100

    pas en suisse

--- roboclic.py
+++ roboclic.py
@@ -290,7 +290,8 @@ def bureau(update, context):
             "Je suis actuellement au bureau", 
             "Je suis à proximité du bureau",
             "Je compte m'y rendre bientôt",
-            "J'y suis pas"
+            "J'y suis pas",
+            "Je suis pas en Suisse"
             ]
     context.bot.send_poll(chat_id=update.effective_chat.id,
                           question=question,
