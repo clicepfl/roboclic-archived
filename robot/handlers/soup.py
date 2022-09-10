96a0a9aa90e48ad085ac80169420017c4ec49d39
Lucas Trognon <lucas.trognon@gmail.com>
2022-09-10 14:21:39 +0200

    shames relou

--- robot/handlers/soup.py
+++ robot/handlers/soup.py
@@ -32,7 +32,6 @@ EMOJIS_FOOD = [
 ]
 VEGETARIAN_WORDS = {"veg", "vege", "vegetarien", "vegetarian"}
 
-
 @dataclass
 class Dish:
     prices: List[float]
@@ -64,8 +63,8 @@ class Menu:
         # removes non-Lausanne results
         excluded = set(["La Ruch", "Microci", "Hodler"])
         # handle edge case names
-        alias = {"La Tabl": "Vallotton", "Maharaj": "Maharaja"}
-
+        alias = {"La Tabl": "Vallotton", "Maharaj": "Maharaja", "Satelli":"Sat"}
+       
         soup = bs(html, "html.parser")
         content = soup.find_all("tr", {"class": "menuPage"})
         dishes = []
@@ -126,6 +125,16 @@ def soup(update, context):
     """
     logger.info(f"user #{update.effective_user.id} required soup ({context.args})")
     now = datetime.now()
+    
+    if "soup_group" not in REQUEST_TIMER:
+       REQUEST_TIMER["soup_group"]=[]
+    if update.message.chat_id in REQUEST_TIMER["soup_group"] and (now-REQUEST_TIMER["soup_group"][update.message.chat_id][0]).total_seconds() < 3600:
+        context.bot.send_message(
+                chat_id=update.message.chat_id,
+                text="🙄",
+                reply_to_message_id=REQUEST_TIMER["soup_group"][update.message.chat_id][1],
+                disable_notification=True)
+        return
     if (
         "soup" not in REQUEST_TIMER
         or (now - REQUEST_TIMER["soup"]).total_seconds() >= 3600
@@ -166,8 +175,9 @@ def soup(update, context):
         if len(menu_filter.filters):
             menu = menu_filter(menu)
 
-    update.message.reply_text(
+   soup_id = update.message.reply_text(
         str(menu),
         quote=False,
         parse_mode=telegram.constants.PARSEMODE_HTML,
-    )
+    ).message_id()
+    REQUEST_TIMER["soup_group"][update.message.chat_id]=(now, soup_id)
